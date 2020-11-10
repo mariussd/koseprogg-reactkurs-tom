2036d27495eb4f27d7b99504903282e07a1e15d0
mariussd <mariussd@users.noreply.github.com>
2020-11-10 10:06:34 +0100

    Yeeha

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./Header";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hei, koseprogg!</h1>
+      <Header />
     </div>
   );
 }
--- src/PostInput.js
+++ src/PostInput.js
@@ -4,55 +4,13 @@ import { imageLinks } from "./imageLinks";
 import "./styles.css";
 
 const PostInput = (props) => {
-  const [text, setText] = React.useState("");
-  const [title, setTitle] = React.useState("");
+  const handleTextChange = (event) => {};
 
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  };
+  const handleTitleChange = (event) => {};
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
+  const handleSubmit = () => {};
 
-  const handleSubmit = () => {
-    if (title === "" || text === "") {
-      alert(
-        "Vennligst fyll ut både tittel og tekst før du legger til et innlegg."
-      );
-      return;
-    }
-
-    props.addPost(
-      title,
-      text,
-      new Date().toLocaleString(),
-      imageLinks[Math.floor(Math.random() * imageLinks.length)]
-    );
-    setTitle("");
-    setText("");
-  };
-
-  return (
-    <div className="PostInput">
-      <h2>Nytt innlegg</h2>
-      <textarea
-        placeholder="Tittel"
-        rows={1}
-        cols={70}
-        value={title}
-        onChange={handleTitleChange}
-      />
-      <textarea
-        placeholder="Tekst"
-        rows={8}
-        cols={70}
-        value={text}
-        onChange={handleTextChange}
-      />
-      <Button title={"Legg til post"} onClick={handleSubmit} />
-    </div>
-  );
+  return <div className="PostInput"></div>;
 };
 
 export default PostInput;
